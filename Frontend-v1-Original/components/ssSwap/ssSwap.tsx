2411589a2041363a7437a8c09c0c9b32c06a3910
torbik <liker1011@gmail.com>
2023-03-06 13:10:47 +0700

    Fix token icons in swap

--- Frontend-v1-Original/components/ssSwap/ssSwap.tsx
+++ Frontend-v1-Original/components/ssSwap/ssSwap.tsx
@@ -1,11 +1,13 @@
 import { Paper } from "@mui/material";
 import Setup from "./setup";
-import classes from "./ssSwap.module.css";
 
 function Swap() {
   return (
-    <div className={classes.newSwapContainer}>
-      <Paper elevation={0} className={classes.swapContainer}>
+    <div className="mt-32 flex h-full min-h-[calc(100vh-432px)] w-full flex-col items-center justify-evenly sm:mt-0 lg:flex-row">
+      <Paper
+        elevation={0}
+        className="flex w-full max-w-[485px] flex-col p-3 shadow-lg shadow-cantoGreen lg:p-6"
+      >
         <Setup />
       </Paper>
     </div>
