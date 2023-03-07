929e3289ff1b55efc298894038d69237afcbf6dc
torbik <liker1011@gmail.com>
2023-03-07 09:46:15 +0700

    remove comment

--- Frontend-v1-Original/components/migration/migration.tsx
+++ Frontend-v1-Original/components/migration/migration.tsx
@@ -6,7 +6,6 @@ const v2 = process.env.NEXT_PUBLIC_V === "v2";
 
 function Migration() {
   return (
-    // lg -> 576 mt is broken
     <div className="mt-32 flex h-full min-h-[calc(100vh-432px)] w-full flex-col items-center justify-evenly gap-8 xs:mt-32 min-[1201px]:mt-0">
       {v2 && (
         <div className="flex max-w-sm flex-col gap-2 rounded-lg border border-cantoGreen bg-black p-4 tracking-wide text-green-100 md:max-w-xl">
