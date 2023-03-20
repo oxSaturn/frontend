f9fb0a0a4f7db17a1e7a12b9c5b4c6a512a98078
torbik <liker1011@gmail.com>
2023-03-20 18:36:08 +0700

    Add warning to votes page about casting votes each epoch

---
+++ Frontend-v1-Original/components/warning/warning.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Dialog } from "@mui/material";
+
+export default function WarningModal({
+  close,
+  acceptWarning,
+}: {
+  close: () => void;
+  acceptWarning: () => void;
+}) {
+  const [accepted, setAccepted] = useState(false);
+
+  const onClose = () => {
+    if (accepted) {
+      acceptWarning();
+    }
+    close();
+  };
+  return (
+    <Dialog open={true} onClose={onClose} className="fixed z-[10030]">
+      <div className="flex max-w-lg flex-col items-center justify-center rounded-[10px] border border-cantoGreen p-10">
+        <img src="/images/icon-warning.svg" className="mb-4 h-10" />
+        <div className="mb-4 text-xl font-extrabold leading-none tracking-tight text-white md:text-2xl lg:text-4xl">
+          Voting information
+        </div>
+        <div className="text-left text-base font-normal text-lime-50">
+          You must cast votes each epoch in order to get voting rewards.
+          <br />
+          UI represents weights of your votes in the pool. Weights do move to
+          the next epoch. However, in order to get voting rewards you need to
+          cast votes each epoch. If you don&apos;t want to change your votes,
+          just press 'Cast Votes' button.
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center gap-1">
+            <label htmlFor="showAgain">Do not show again</label>
+            <input
+              id="showAgain"
+              type="checkbox"
+              onChange={(e) => setAccepted(e.target.checked)}
+            />
+          </div>
+          <button
+            onClick={onClose}
+            className="mr-2 mb-2 rounded-lg border border-cantoGreen px-5 py-2.5 text-center text-sm font-medium text-cantoGreen transition-colors hover:bg-green-900 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
