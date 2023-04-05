7b1ca1c8f933bde3dc9ee91aa099ce37172a8a0b
oxSaturn <oxSaturn@proton.me>
2023-04-05 10:11:12 +0800

    add api for getting flow circulating supply on canto

---
+++ Frontend-v1-Original/pages/api/canto/circulating-supply.ts
@@ -0,0 +1,312 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createPublicClient, getContract, http } from "viem";
+import { canto } from "viem/chains";
+import { CONTRACTS, NATIVE_TOKEN } from "../../../stores/constants/constants";
+
+const publicClient = createPublicClient({
+  chain: canto,
+  transport: http(),
+});
+
+const flowContract = getContract({
+  address: CONTRACTS.GOV_TOKEN_ADDRESS as `0x${string}`,
+  // I'm inlining the abi here to enable type inference for typescript
+  // see https://viem.sh/docs/typescript.html#type-inference
+  abi: [
+    {
+      inputs: [{ internalType: "address", name: "_router", type: "address" }],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "address",
+          name: "spender",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "oldVault",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "newVault",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "effectiveTime",
+          type: "uint256",
+        },
+      ],
+      name: "LogChangeVault",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "value",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      inputs: [],
+      name: "DOMAIN_SEPARATOR",
+      outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "PERMIT_TYPEHASH",
+      outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "", type: "address" },
+        { internalType: "address", name: "", type: "address" },
+      ],
+      name: "allowance",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_spender", type: "address" },
+        { internalType: "uint256", name: "_value", type: "uint256" },
+      ],
+      name: "approve",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "account", type: "address" },
+        { internalType: "uint256", name: "amount", type: "uint256" },
+      ],
+      name: "burn",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_pendingRouter", type: "address" },
+      ],
+      name: "changeVault",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "decimals",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "account", type: "address" },
+        { internalType: "uint256", name: "amount", type: "uint256" },
+      ],
+      name: "mint",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "minter",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "", type: "address" }],
+      name: "nonces",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "pendingRouter",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "pendingRouterDelay",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "owner", type: "address" },
+        { internalType: "address", name: "spender", type: "address" },
+        { internalType: "uint256", name: "value", type: "uint256" },
+        { internalType: "uint256", name: "deadline", type: "uint256" },
+        { internalType: "uint8", name: "v", type: "uint8" },
+        { internalType: "bytes32", name: "r", type: "bytes32" },
+        { internalType: "bytes32", name: "s", type: "bytes32" },
+      ],
+      name: "permit",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "router",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "_minter", type: "address" }],
+      name: "setMinter",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalSupply",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_to", type: "address" },
+        { internalType: "uint256", name: "_value", type: "uint256" },
+      ],
+      name: "transfer",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "_from", type: "address" },
+        { internalType: "address", name: "_to", type: "address" },
+        { internalType: "uint256", name: "_value", type: "uint256" },
+      ],
+      name: "transferFrom",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ],
+  publicClient: publicClient,
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const totalSupply = await flowContract.read.totalSupply();
+  const lockedSupply = await flowContract.read.balanceOf([
+    CONTRACTS.VE_TOKEN_ADDRESS as `0x${string}`,
+  ]);
+  const flowInMinter = await flowContract.read.balanceOf([
+    CONTRACTS.MINTER_ADDRESS as `0x${string}`,
+  ]);
+  const flowInMsig = await flowContract.read.balanceOf([
+    CONTRACTS.MSIG_ADDRESS as `0x${string}`,
+  ]);
+  const flowInRewardsDistributor = await flowContract.read.balanceOf([
+    CONTRACTS.VE_DIST_ADDRESS as `0x${string}`,
+  ]);
+  const flowInTimelockerController = await flowContract.read.balanceOf([
+    "0xd0cC9738866cd82B237A14c92ac60577602d6c18",
+  ]);
+
+  // per https://docs.velocimeter.xyz/tokenomics#emission-schedule
+  // the max supply of $flow would be 1,400,000,000 or so
+  // while Number.MAX_SAFE_INTEGER is 9,007,199,254,740,991
+  // so we're safe to use Number to convert Bigint here
+  const circulatingSupply =
+    Number(
+      totalSupply -
+        lockedSupply -
+        flowInMinter -
+        flowInMsig -
+        flowInRewardsDistributor -
+        flowInTimelockerController
+    ) / Math.pow(10, NATIVE_TOKEN.decimals);
+
+  res.status(200).json(circulatingSupply);
+}
