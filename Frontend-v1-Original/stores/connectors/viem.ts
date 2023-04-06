6b1e7f123e782d6aa03573ec413953e634633476
torbik <liker1011@gmail.com>
2023-04-06 09:34:56 +0700

    remove neobase from fallback as it brings empty data, add chandrastation

--- Frontend-v1-Original/stores/connectors/viem.ts
+++ Frontend-v1-Original/stores/connectors/viem.ts
@@ -5,15 +5,20 @@ const dexvaults = http("https://canto.dexvaults.com");
 const plexnode = http("https://mainnode.plexnode.org:8545");
 const nodestake = http("https://jsonrpc.canto.nodestake.top");
 const slingshot = http("https://canto.slingshot.finance");
-const neobase = http("https://canto.neobase.one");
+const chandrastation = http("https://canto.evm.chandrastation.com/");
+// going to remove neobase because it sends back empty data when can't fetch, this breaks the fallback
+// const neobase = http("https://canto.neobase.one");
 
 const client = createPublicClient({
   chain: canto,
-  transport: fallback([dexvaults, plexnode, nodestake, slingshot, neobase], {
-    rank: {
-      interval: 30_000,
-    },
-  }),
+  transport: fallback(
+    [dexvaults, plexnode, nodestake, slingshot, chandrastation],
+    {
+      rank: {
+        interval: 30_000,
+      },
+    }
+  ),
 });
 
 export default client;
