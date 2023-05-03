ae55bd890bfe845d8d355c072f415bed288c82a2
t0rbik <liker1011@gmail.com>
2023-05-03 11:55:08 +0700

    init launchapd

---
+++ Frontend-v1-Original/pages/launchpad/[address]/index.tsx
@@ -0,0 +1,24 @@
+function LaunchpadProject() {
+  return (
+    <div className="h-100 w-100 mb-16 mt-0 flex flex-col pt-8">
+      get the launchpad project by address fetch time left total raised project
+      token price (either initial or current if overflow)
+      <br />
+      circ supply and fdv if needed
+      <br />
+      referral link
+      <br />
+      read ref code in url
+      <br />
+      input amount
+      <br />
+      based on has ended approve/buy or claim
+      <br />
+      approve/buy
+      <br />
+      claim
+    </div>
+  );
+}
+
+export default LaunchpadProject;
