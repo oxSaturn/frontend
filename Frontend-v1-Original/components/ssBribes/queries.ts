4081419f0b59c9f4548400a2ccd93e1764d130ef
t0rbik <liker1011@gmail.com>
2023-05-07 14:21:02 +0700

    add eth cre8r autobribe

--- Frontend-v1-Original/components/ssBribes/queries.ts
+++ Frontend-v1-Original/components/ssBribes/queries.ts
@@ -8,6 +8,10 @@ const autoBribesAddresses = [
   "0x5318FfE879e6027fD009beA6837E21F40EEf3903",
   "0xb091b7816112d870609Ca1c6E64bD140c189BA34",
   "0x4bC90701a5f3e72A5Fe2686C62Da24B20ca1cfB6",
+  "0x0CD1b0fAB074727D7504c9Dc23f131598cFE5427",
+  // not deposited
+  // "0x8dcf1a8086e700eab31936af234b2d1212f257db",
+  // "0xd2b0933EaD44592A7867f00DC1D3Dcdfe3806b83",
 ] as const;
 
 const getAutoBribes = async () => {
