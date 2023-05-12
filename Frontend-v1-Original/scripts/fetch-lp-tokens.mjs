aec776139605a8ed0d0cb2730869bb2dd8d3908c
oxSaturn <oxSaturn@proton.me>
2023-05-12 10:48:44 +0800

    add a script

---
+++ Frontend-v1-Original/scripts/fetch-lp-tokens.mjs
@@ -0,0 +1,111 @@
+import { createPublicClient, http } from "viem";
+import { canto } from "viem/chains";
+const client = createPublicClient({
+  chain: canto,
+  transport: http(),
+});
+const pairFactoryAddress = "0xF80909DF0A01ff18e4D37BF682E40519B21Def46";
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "allPairs",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "allPairsLength",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+const pairsLength = await client.readContract({
+  address: pairFactoryAddress,
+  abi,
+  functionName: "allPairsLength",
+});
+const multicallAddress = "0xcA11bde05977b3631167028862bE2a173976CA11";
+const pairs = await client.multicall({
+  multicallAddress,
+  contracts: Array.from({ length: Number(pairsLength) }, (_, i) => ({
+    address: pairFactoryAddress,
+    abi,
+    functionName: "allPairs",
+    args: [i],
+  })),
+});
+const lpTokens = pairs.map(({ result }) => result);
+let tokens = await client.multicall({
+  multicallAddress,
+  contracts: lpTokens
+    .map((address) => [
+      {
+        address,
+        abi: [
+          {
+            constant: true,
+            inputs: [],
+            name: "token0",
+            outputs: [
+              {
+                internalType: "address",
+                name: "",
+                type: "address",
+              },
+            ],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+        ],
+        functionName: "token0",
+      },
+      {
+        address,
+        abi: [
+          {
+            constant: true,
+            inputs: [],
+            name: "token1",
+            outputs: [
+              {
+                internalType: "address",
+                name: "",
+                type: "address",
+              },
+            ],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+        ],
+        functionName: "token1",
+      },
+    ])
+    .flat(),
+});
+tokens = new Set(tokens.map((token) => token.result));
+// now we have a list of token addresses
+// TODO fetch data from coingecko api so users can visit the token page on coingecko?
+// Public API has a rate limit of 10-30 calls/minute, and doesn't come with API key
+// https://api.coingecko.com/api/v3/coins/canto/contract/0x7264610A66EcA758A8ce95CF11Ff5741E1fd0455
