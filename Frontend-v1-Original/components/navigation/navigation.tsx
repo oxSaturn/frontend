2bb9537d1eacab89cd41e69a6331816fa33e2ef1
t0rbik <liker1011@gmail.com>
2023-05-23 21:18:35 +0700

    update link button

--- Frontend-v1-Original/components/navigation/navigation.tsx
+++ Frontend-v1-Original/components/navigation/navigation.tsx
@@ -58,7 +58,7 @@ function Navigation() {
             active === link
               ? "border-cantoGreen text-white"
               : "border-transparent"
-          } inline-flex items-center justify-center py-6 hover:bg-[hsla(0,0%,100%,.04)]`}
+          } inline-flex items-center justify-center hover:bg-[hsla(0,0%,100%,.04)]`}
         >
           <div className="m-0 pl-0 text-center text-xs xs:text-base">
             {title}
@@ -71,7 +71,7 @@ function Navigation() {
   const renderDocsTab = () => {
     return (
       <button
-        className="relative m-0 inline-flex cursor-pointer select-none appearance-none items-center justify-center rounded-lg border border-transparent bg-transparent px-[24px] py-6 pt-2 pb-[10px] text-sm font-medium capitalize text-secondaryGray no-underline outline-0 hover:bg-[hsla(0,0%,100%,.04)]"
+        className="relative m-0 inline-flex cursor-pointer select-none appearance-none items-center justify-center rounded-lg border border-transparent bg-transparent px-[24px] pt-2 pb-[10px] text-sm font-medium capitalize text-secondaryGray no-underline outline-0 hover:bg-[hsla(0,0%,100%,.04)]"
         onClick={() => window.open("https://docs.velocimeter.xyz/", "_blank")}
       >
         <div className="m-0 pl-0 text-center text-xs xs:text-base">Docs</div>
@@ -82,7 +82,7 @@ function Navigation() {
   const renderScantoTab = () => {
     return (
       <button
-        className="relative m-0 inline-flex cursor-pointer select-none appearance-none items-center justify-center gap-1 rounded-lg border border-transparent bg-transparent px-[24px] py-6 pt-2 pb-[10px] text-sm font-medium text-secondaryGray no-underline outline-0 hover:bg-[hsla(0,0%,100%,.04)]"
+        className="relative m-0 inline-flex cursor-pointer select-none appearance-none items-center justify-center gap-1 rounded-lg border border-transparent bg-transparent p-1 text-sm font-medium text-secondaryGray no-underline outline-0 hover:bg-[hsla(0,0%,100%,.04)]"
         onClick={() => window.open("https://www.scanto.io/", "_blank")}
       >
         <Image
@@ -91,9 +91,7 @@ function Navigation() {
           height={40}
           alt="sCANTO Token Icon"
         />
-        <div className="m-0 pl-0 text-center text-xs xs:text-base">
-          Mint sCANTO
-        </div>
+        <div className="m-0 pl-0 text-center text-xs xs:text-base">sCANTO</div>
       </button>
     );
   };
