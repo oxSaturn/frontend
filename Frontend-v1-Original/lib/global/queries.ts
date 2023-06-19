4436eab5a1ec06e18174e31267ccdad8567616ca
t0rbik <liker1011@gmail.com>
2023-06-19 19:31:26 +0700

    hotfix apr

--- Frontend-v1-Original/lib/global/queries.ts
+++ Frontend-v1-Original/lib/global/queries.ts
@@ -476,6 +476,18 @@ export const getPairsWithGauges = async (
         functionName: "weights",
         args: [pair.address],
       },
+      {
+        address: pair.gauge.address,
+        abi: CONTRACTS.GAUGE_ABI,
+        functionName: "left",
+        args: [CONTRACTS.GOV_TOKEN_ADDRESS],
+      },
+      {
+        address: pair.gauge.address,
+        abi: CONTRACTS.GAUGE_ABI,
+        functionName: "left",
+        args: [CONTRACTS.OPTION_TOKEN],
+      },
     ] as const;
   });
   const gaugesCallsChunks = chunkArray(gaugesCalls);
@@ -487,10 +499,8 @@ export const getPairsWithGauges = async (
     if (hasGauge(pair)) {
       const isAliveGauge = gaugesAliveData[outerIndex];
 
-      const [totalSupply, gaugeBalance, gaugeWeight] = gaugesData.slice(
-        outerIndex * 3,
-        outerIndex * 3 + 3
-      );
+      const [totalSupply, gaugeBalance, gaugeWeight, flowLeft, optionLeft] =
+        gaugesData.slice(outerIndex * 5, outerIndex * 5 + 5);
 
       pair.gauge.balance = formatEther(gaugeBalance);
       pair.gauge.totalSupply = formatEther(totalSupply);
@@ -519,6 +529,8 @@ export const getPairsWithGauges = async (
       pair.gaugebribes = pair.gauge.bribes;
       pair.isAliveGauge = isAliveGauge;
       if (isAliveGauge === false) pair.apr = 0;
+      if (flowLeft === 0n) pair.apr = 0;
+      if (optionLeft === 0n) pair.oblotr_apr = 0;
 
       outerIndex++;
     }
