1078c278531faaa3d6c5913f5ea55791faea035a
t0rbik <liker1011@gmail.com>
2023-06-30 14:07:05 +0700

    add deployed contracts

--- Frontend-v1-Original/stores/constants/contracts.ts
+++ Frontend-v1-Original/stores/constants/contracts.ts
@@ -1,9 +1,9 @@
 import abis from "../abis/abis";
 
 export const OPTION_TOKEN_ADDRESS =
-  "0x1Fc0A9f06B6E85F023944e74F70693Ac03fDC621"; // pulse
+  "0xfEFBC02930D389E624B56053E345Cd848729BF93"; // fantom
 
-export const GOV_TOKEN_ADDRESS = "0x39b9D781dAD0810D07E24426c876217218Ad353D"; // pulse
+export const GOV_TOKEN_ADDRESS = "0xE1689e9AaD6b36D91E357FAf95cd7c2C4C1b5475"; // fantom
 export const GOV_TOKEN_NAME = "FVM";
 export const GOV_TOKEN_SYMBOL = "FVM";
 export const GOV_TOKEN_DECIMALS = 18;
@@ -11,7 +11,7 @@ export const GOV_TOKEN_LOGO =
   "https://raw.githubusercontent.com/Velocimeter/frontend/fantom/Frontend-v1-Original/public/tokens/fvm.png?raw=true";
 export const GOV_TOKEN_ABI = abis.tokenABI;
 
-export const VE_TOKEN_ADDRESS = "0xe7b8F4D74B7a7b681205d6A3D231d37d472d4986"; // pulse
+export const VE_TOKEN_ADDRESS = "0x34172780901eF075C67942392Ab461186d8C8cc5"; // fantom
 export const VE_TOKEN_NAME = "veNFT";
 export const VE_TOKEN_SYMBOL = "veNFT";
 export const VE_TOKEN_DECIMALS = 18;
@@ -19,7 +19,7 @@ export const VE_TOKEN_LOGO =
   "https://cre8r.vip/wp-content/uploads/2023/02/Flow-circle-aqua-150x150.png";
 export const VE_TOKEN_ABI = abis.veTokenABI;
 
-export const WETH_ADDRESS = "0xA1077a294dDE1B09bB078844df40758a5D0f9a27"; // pulse
+export const WETH_ADDRESS = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"; // fantom
 export const WETH_NAME = "Wrapped FTM";
 export const WETH_SYMBOL = "WFTM";
 export const WETH_DECIMALS = 18;
@@ -32,19 +32,19 @@ export const ETH_DECIMALS = 18;
 export const ETH_LOGO =
   "https://assets.coingecko.com/coins/images/4001/large/Fantom_round.png";
 
-export const FACTORY_ADDRESS = "0x6B4449C74a9aF269A5f72B88B2B7B8604685D9B9"; // pulse
+export const FACTORY_ADDRESS = "0x42a0B204ae55254CdD10c562F9b101248Ea5DE08"; // fantom
 export const FACTORY_ABI = abis.factoryABI;
 
-export const ROUTER_ADDRESS = "0x370d160992C8C48BCCFcf009f0c9db9d00574eF7"; // pulse
+export const ROUTER_ADDRESS = "0x197b8F8185B84112dD95527ce472A55052cEE030"; // fantom
 export const ROUTER_ABI = abis.routerABI;
 
-export const VE_DIST_ADDRESS = "0x582aEB28632800467C7F672375fE57baB15822a5"; // pulse
+export const VE_DIST_ADDRESS = "0x82D862D11D4Ff392c1BaeF5502DaA57e99A9E339"; // fantom
 export const VE_DIST_ABI = abis.veDistABI;
 
-export const VOTER_ADDRESS = "0x8C4FF4004c8a85054639B86E9F8c26e9DA7ff738"; // pulse
+export const VOTER_ADDRESS = "0x1E7d4DaFA8E16F90fd5B1220059607fe1f69aA0C"; // fantom
 export const VOTER_ABI = abis.voterABI;
 
-export const MINTER_ADDRESS = "0x1D84F65DAe4bf9298be27d62cB06A3b32f79fDCC"; // pulse
+export const MINTER_ADDRESS = "0xCbC293D3fA882A5740A9fCD4670d69FD3f5Fe788"; // fantom
 export const MINTER_ABI = abis.minterABI;
 
 export const ERC20_ABI = abis.erc20ABI;
@@ -58,8 +58,8 @@ export const AUTO_BRIBE_ABI = abis.autoBribeABI;
 export const FAIR_AUCTION_ABI = abis.fairAuctionABI;
 
 export const STABLE_TOKEN_ADDRESS =
-  "0x15D38573d2feeb82e7ad5187aB8c1D52810B1f07"; // pulse
+  "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75"; // fantom
 
-export const MSIG_ADDRESS = "0xa3082df7a11071db5ed0e02d48bca5f471ddbaf4"; // pulse
+export const MSIG_ADDRESS = "0x88Dec6df03C2C111Efd4ad89Cef2c0347034AFC0"; // fantom
 
 //LFG
