4292911f9dfc6ea72e607b369714dc4ca5cde82f
oxSaturn <oxSaturn@proton.me>
2023-07-02 21:28:17 +0800

    fix import

--- Frontend-v1-Original/components/navigation/navigation.tsx
+++ Frontend-v1-Original/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect, type ReactNode } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
