09211044090286c04201e2ebabc827c60a995b8b
oxSaturn <oxSaturn@proton.me>
2023-07-03 22:32:37 +0800

    use 127.0.0.1

--- Frontend-v1-Original/playwright.config.ts
+++ Frontend-v1-Original/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "PORT=3000 npm run dev",
-    url: "http://localhost:3000",
+    url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // chances are next.js is slow to start on CI because of low resources
     // see https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
