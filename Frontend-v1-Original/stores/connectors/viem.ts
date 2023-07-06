3b9fbda002a69c7e6d7eaeae94b3e3ad89ed48e1
oxSaturn <oxSaturn@proton.me>
2023-07-06 20:19:10 +0800

    set fallback

--- Frontend-v1-Original/stores/connectors/viem.ts
+++ Frontend-v1-Original/stores/connectors/viem.ts
@@ -43,7 +43,7 @@ const { chains, publicClient } = configureChains(
   [
     jsonRpcProvider({
       rpc: () => ({
-        http: process.env.NEXT_PUBLIC_RPC_URL!,
+        http: process.env.NEXT_PUBLIC_RPC_URL ?? 'https://canto.gravitychain.io',
       }),
     }),
     jsonRpcProvider({
