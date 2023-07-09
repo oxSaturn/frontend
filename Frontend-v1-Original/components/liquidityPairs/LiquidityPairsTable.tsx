d37c3153945d967ad116492e090dc46e051f69b8
t0rbik <liker1011@gmail.com>
2023-07-09 18:40:27 +0700

    move apr and tvl to pair name

--- Frontend-v1-Original/components/liquidityPairs/LiquidityPairsTable.tsx
+++ Frontend-v1-Original/components/liquidityPairs/LiquidityPairsTable.tsx
@@ -43,6 +43,18 @@ const headCells = [
   { id: "expand", numeric: false, disablePadding: true, label: "" },
   { id: "pair", numeric: false, disablePadding: false, label: "Pair" },
   {
+    id: "apr",
+    numeric: true,
+    disablePadding: false,
+    label: "APR",
+  },
+  {
+    id: "tvl",
+    numeric: true,
+    disablePadding: false,
+    label: "TVL",
+  },
+  {
     id: "balance",
     numeric: true,
     disablePadding: false,
@@ -71,18 +83,6 @@ const headCells = [
     numeric: true,
     disablePadding: false,
     label: "Total Pool Staked",
-  },
-  {
-    id: "tvl",
-    numeric: true,
-    disablePadding: false,
-    label: "TVL",
-  },
-  {
-    id: "apr",
-    numeric: true,
-    disablePadding: false,
-    label: "APR",
   },
   {
     id: "",
@@ -554,6 +554,23 @@ function Row(props: { row: Pair; onView: (_row: Pair) => void }) {
             </div>
           </div>
         </TableCell>
+        <TableCell align="right">
+          <div className="flex items-center justify-end gap-1">
+            {row.isAliveGauge === false && (
+              <Tooltip title="Gauge has been killed">
+                <WarningOutlined className="ml-2 text-base text-yellow-300" />
+              </Tooltip>
+            )}
+            <Typography variant="h2" className="text-xs font-extralight">
+              {(row.apr + row.oblotr_apr).toFixed(2)}%
+            </Typography>
+          </div>
+        </TableCell>
+        <TableCell className="max-2xl:hidden" align="right">
+          <Typography variant="h2" className="text-xs font-extralight">
+            {formatTVL(row.tvl)}
+          </Typography>
+        </TableCell>
         <TableCell className="max-md:hidden" align="right">
           {row &&
             row.token0 &&
@@ -843,23 +860,6 @@ function Row(props: { row: Pair; onView: (_row: Pair) => void }) {
             </Typography>
           </TableCell>
         )}
-        <TableCell className="max-2xl:hidden" align="right">
-          <Typography variant="h2" className="text-xs font-extralight">
-            {formatTVL(row.tvl)}
-          </Typography>
-        </TableCell>
-        <TableCell align="right">
-          <div className="flex items-center justify-end gap-1">
-            {row.isAliveGauge === false && (
-              <Tooltip title="Gauge has been killed">
-                <WarningOutlined className="ml-2 text-base text-yellow-300" />
-              </Tooltip>
-            )}
-            <Typography variant="h2" className="text-xs font-extralight">
-              {(row.apr + row.oblotr_apr).toFixed(2)}%
-            </Typography>
-          </div>
-        </TableCell>
         <TableCell align="right">
           <Button
             variant="outlined"
