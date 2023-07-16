412b32dd20dfd36c75a1bd38f60242819ed6887c
oxSaturn <oxSaturn@proton.me>
2023-07-16 12:56:01 +0800

    clean up vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { configDefaults, defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./scripts/env-setup.ts"],
