8e1b3dcc049a8cd2d65b35e947beef384ac875ec
oxSaturn <oxSaturn@proton.me>
2023-07-19 09:11:47 +0800

    keep select

--- pages/options/[[...option]].tsx
+++ pages/options/[[...option]].tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Link from "next/link";
 
 import { Redeem } from "../../components/options/redeem";
 import { Stake } from "../../components/options/stake";
@@ -8,27 +9,32 @@ import { Reward } from "../../components/options/reward";
 import { Convert } from "../../components/options/convert";
 import { PageWrapper } from "../../components/common/PageWrapper";
 import { useInputs } from "../../components/options/lib/useInputs";
+import { Select, SelectItem } from "../../components/common/radixSelect";
 import {
   OPTIONS,
   type oToken as OptionToken,
 } from "../../stores/constants/constants";
 
+function transformOptionQuery(
+  option: string | string[] | undefined
+): OptionToken {
+  if (typeof option === "undefined") {
+    return "oFVM";
+  }
+  if (Array.isArray(option)) {
+    const [oToken] = option;
+    if (Object.keys(OPTIONS).includes(oToken)) {
+      return oToken as OptionToken;
+    }
+  }
+  return "oFVM";
+}
 function Options() {
-  const { option } = useRouter().query;
+  const router = useRouter();
+  const { option } = router.query;
   const { setOptionToken } = useInputs();
   useEffect(() => {
-    // visit /options
-    if (typeof option === "undefined") {
-      setOptionToken("oFVM");
-      return;
-    }
-    // visit /options/oToken
-    if (Array.isArray(option)) {
-      const [oToken] = option;
-      if (Object.keys(OPTIONS).includes(oToken)) {
-        setOptionToken(oToken as OptionToken);
-      }
-    }
+    setOptionToken(transformOptionQuery(option));
   }, [option, setOptionToken]);
   return (
     <PageWrapper
@@ -49,12 +55,28 @@ function Options() {
         </div>
       }
     >
-      <div className="flex flex-wrap items-stretch justify-center gap-5">
-        <Redeem />
-        <div className="flex flex-col items-center justify-between gap-1">
-          <Stake />
-          <Reward />
-          <Convert />
+      <div className="space-y-5">
+        <h1 className="text-center text-2xl flex justify-center items-center">
+          <Select
+            value={transformOptionQuery(option)}
+            onValueChange={(value) => {
+              router.push(`/options/${value}`);
+            }}
+          >
+            {Object.keys(OPTIONS).map((oToken) => (
+              <SelectItem key={oToken} value={oToken}>
+                {oToken}
+              </SelectItem>
+            ))}
+          </Select>
+        </h1>
+        <div className="flex flex-wrap items-stretch justify-center gap-5">
+          <Redeem />
+          <div className="flex flex-col items-center justify-between gap-1">
+            <Stake />
+            <Reward />
+            <Convert />
+          </div>
         </div>
       </div>
     </PageWrapper>
