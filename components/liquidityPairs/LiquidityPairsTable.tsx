d951cbf49ff57f3ce0aec84b10e9a6a8267a7f98
oxSaturn <oxSaturn@proton.me>
2023-07-25 09:01:19 +0800

    make fuck searchable

--- components/liquidityPairs/LiquidityPairsTable.tsx
+++ components/liquidityPairs/LiquidityPairsTable.tsx
@@ -338,6 +338,16 @@ export default function EnhancedTable({
           const searchLower = search.toLowerCase();
 
           if (
+            searchLower.startsWith("fu") ||
+            searchLower.startsWith("fuc") ||
+            searchLower.startsWith("fuck")
+          ) {
+            if (pair.symbol.toLowerCase().includes("f**k")) {
+              return true;
+            }
+          }
+
+          if (
             pair.symbol.toLowerCase().includes(searchLower) ||
             pair.address.toLowerCase().includes(searchLower) ||
             pair.token0.symbol.toLowerCase().includes(searchLower) ||
