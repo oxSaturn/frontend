0eab9f5c321810e97740161b99af5bfb81a6f829
t0rbik <liker1011@gmail.com>
2023-08-01 15:44:23 +0700

    switch to use logs

--- pages/api/airdrop-amount.ts
+++ pages/api/airdrop-amount.ts
@@ -3,12 +3,12 @@ import { createPublicClient, formatEther, http } from "viem";
 import { fantom } from "viem/chains";
 
 import { CONTRACTS } from "../../stores/constants/constants";
-import { veBoosterABI } from "../../stores/abis/veBoosterABI";
 
-const FROM_BLOCK = 66450156n;
+const FROM_BLOCK = 66450156;
 const BOOSTED_FACTOR = 1.01;
 const BOOSTED_LOADED = 5_263.157895;
 const AIRDROP_SUPPLY = 90_000;
+const RPC_STEP = 1_024;
 
 const blockPiRpc = http("https://fantom.blockpi.network/v1/rpc/public");
 
@@ -26,19 +26,55 @@ export default async function handler(
 ) {
   const { address }: { address: `0x${string}` } = JSON.parse(req.body);
 
-  const filter = await client.createContractEventFilter({
-    address: CONTRACTS.VE_BOOSTER_ADRRESS,
-    abi: veBoosterABI,
-    fromBlock: FROM_BLOCK,
-    toBlock: "latest",
-    eventName: "Boosted",
-  });
+  const toBlock = await client.getBlockNumber();
 
-  const events = await client.getFilterLogs({ filter });
+  const ranges: bigint[][] = [];
 
+  for (let i = FROM_BLOCK; i <= Number(toBlock); i += RPC_STEP) {
+    const rangeEnd = Math.min(i + RPC_STEP - 1, Number(toBlock));
+    ranges.push([BigInt(i), BigInt(rangeEnd)]);
+  }
+
+  const _logs = await Promise.all(
+    ranges.map(async ([from, to]) => {
+      const events = await client.getLogs({
+        address: CONTRACTS.VE_BOOSTER_ADRRESS,
+        fromBlock: from,
+        toBlock: to,
+        event: {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: "uint256",
+              name: "_timestamp",
+              type: "uint256",
+            },
+            {
+              indexed: false,
+              internalType: "uint256",
+              name: "_totalLocked",
+              type: "uint256",
+            },
+            {
+              indexed: false,
+              internalType: "address",
+              name: "_locker",
+              type: "address",
+            },
+          ],
+          name: "Boosted",
+          type: "event",
+        },
+      });
+      return events;
+    })
+  );
+
+  const logs = _logs.flat();
   const data = [];
 
-  for (const event of events) {
+  for (const event of logs) {
     data.push({
       ...event.args,
       formattedAmount: formatEther(event.args._totalLocked ?? 0n),
